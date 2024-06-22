28814791afb9d354f3e0b6d23e0a48713ee6ee0d
sindhuKu3 <sindhuku3@gmail.com>
2024-06-22 18:49:39 +0530

    index

--- index.js
+++ index.js
@@ -135,26 +135,7 @@ passport.deserializeUser(function (user, cb) {
     return cb(null, user);
   });
 });
-//----deployement purpose-------------
-const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "development") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-
-
-
-
-//----deployement purpose-------------
 const server = () => {
   db();
   app.listen(PORT, () => {
